7133bfde40c9a435abae22f966e0b9d5bd1ee126
MAALOUM-SAID <saaid_120@hotmail.com>
2022-04-05 13:52:16 +0000

    v2

--- Todo/views.py
+++ Todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .form import *
 from .models import Task
 # Create your views here.
@@ -7,7 +7,6 @@ def index(request):
     task=Task.objects.all()
     return render(request,'pages/index.html',{'task':task})
 def users(request):
-    err='false'
     if request.method=='POST':
         task_title=TitleForm(request.POST)
         task=ContentForm(request.POST)
@@ -19,27 +18,21 @@ def users(request):
              and begin.is_valid() and end.is_valid()\
              and category.is_valid() and category.is_valid():
             task_title=request.POST.get('title')
-            task=request.POST.get('task')
-            category_post=request.POST.get('category')
-            status_post=request.POST.get('status')
-            begin=category.cleaned_data.get('begin')
-            end=category.cleaned_data.get('end')
-            if True:
-                print("InSIde")
-                data=Task(
-                    task_title=task_title,
-                    task=task,
-                    status=status_post,
-                    category=category_post,
-                    begin=begin,
-                    end=end
-                )
-                data.save()
-            else:
-                print("Outside")
-                err='true'
-
-
+            # task=request.POST.get('task')
+            # category_post=request.POST.get('category')
+            # status_post=request.POST.get('status')
+            # begin=category.cleaned_data.get('begin')
+            # end=category.cleaned_data.get('end')
+            data=Task(
+                task_title=task_title,
+                task=task.cleaned_data.get('task'),
+                status=request.POST.get('status'),
+                category=request.POST.get('category'),
+                begin=begin.cleaned_data.get('begin'),
+                end=end.cleaned_data.get('end')
+            )
+            data.save()
+            return redirect(to='index')
     # Forms
     title=TitleForm()
     content=ContentForm()
@@ -54,7 +47,6 @@ def users(request):
         'status':status,
         'begin':time_begin,
         'end':time_end,
-        'err':err
     }
     return render(request,'pages/users.html',context)
 def register(request):
